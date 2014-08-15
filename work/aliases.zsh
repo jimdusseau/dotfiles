4c0a8484d4bcbd48b2dd58cf136278dcdf599e71
Jim Dusseau <jdusseau@evernote.com>
2014-08-15 09:58:46 -0700

    Slight modifications to superloc
    Change-Id: Ic5e9c40977baea1918f09729cbb98d393f9e24d8

--- work/aliases.zsh
+++ work/aliases.zsh
@@ -1,3 +1,3 @@
 alias chid="git log -n 1 | tail -n 1 | sed 's/^[ \t]*//;s/[ \t]*$//' | pbcopy"
 
-#cd ~/Projects/ios/ios-i18n && gp && cd ~/Projects/ios/ios && gsup && tools/incremental_localize.py . ../ios-i18n/App
+#cd ~/Projects/ios/ios-i18n && gp && cd ~/Projects/ios && gsup && tools/incremental_localize.py . ../ios-i18n/App
