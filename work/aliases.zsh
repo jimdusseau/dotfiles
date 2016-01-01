b6710697a017378978d1cf4b1b1755a509ad7594
Jim Dusseau <jimdusseau@gmail.com>
2016-01-01 13:31:03 -0800

    Removed superloc

--- work/aliases.zsh
+++ work/aliases.zsh
@@ -1,3 +1,1 @@
 alias chid="git log -n 1 | tail -n 1 | sed 's/^[ \t]*//;s/[ \t]*$//' | pbcopy"
-
-alias superloc="cd ~/Projects/ios-i18n && gp && cd - && gsup && tools/incremental_localize.py . ../ios-i18n/App"
